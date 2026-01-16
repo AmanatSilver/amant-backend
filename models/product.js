c02db8e6c42d56219b1389744dcfb14ceb422768
Ayushx13 <ayushpofficialx@gmail.com>
2026-01-17 04:20:34 +0530

    Database setUp

---
+++ models/product.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const { Schema , model} = mongoose ;
+
+const productSchema = new Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    
+})
--- server.js
+++ server.js
@@ -1,7 +1,17 @@
-import {app} from './app.js';
+import "./config.js";
+import { app } from './app.js';
+import mongoose from 'mongoose';
 
 const PORT = process.env.PORT || 7000;
 
+
+//connect to MongoDB
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB).then(async con => {
+  console.log('Connection Successful to AMANAT Database !! 🎉');
+});
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port 🏠 ${PORT}`);
 });
